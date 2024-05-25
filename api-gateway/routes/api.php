ae7efe2e269b66896a836f9fe21945f1dbd5d3e2
MaiLear <maardila847@misena.edu.co>
2024-05-25 18:55:57 -0500

    feat: i added rate limitting in the api

--- api-gateway/routes/api.php
+++ api-gateway/routes/api.php
@@ -10,8 +10,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::resource('products',ProductController::class)->except(['edit','create']);
+Route::resource('products', ProductController::class)->except(['edit', 'create'])->middleware(["throttle:manageSale"]);
 
-Route::resource('sales',SaleController::class)->except(['edit','destroy','create']);
+Route::resource('sales', SaleController::class)->except(['edit', 'destroy', 'create'])->middleware(["throttle:manageSale"]);
 
-Route::resource('presales',PresaleController::class)->except(['edit','destroy','update','create']);
+Route::resource('presales', PresaleController::class)->except(['edit', 'destroy', 'update', 'create'])->middleware(["throttle:manageSale"]);
